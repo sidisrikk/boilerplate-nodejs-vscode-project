3c3c6576c207626e739b9fda429d49b355cae963
wasuthep <wasuthep@arterex.com>
2018-09-30 04:28:54 +0700

    rename test

--- src/getremotedata/getcomment.test.js
+++ src/getremotedata/getcomment.test.js
@@ -4,11 +4,11 @@ describe('call rest api /comments/:id', () => {
   test.each([
     [1, 'some comment'],
     [2, 'random comment'],
-  ])('should ', async (commentId, expected) => {
+  ])('comment id %i ', async (commentId, expected) => {
     const res = await getComment(commentId);
     expect(res.data.body).toBe(expected);
   });
-  test('comment doesn\'t exist', async () => {
+  test('comment 99 doesn\'t exist', async () => {
     const res = await getComment(99);
     expect(res).toBeNull();
   });
--- src/getremotedata/sum.test.js
+++ src/getremotedata/sum.test.js
@@ -1,7 +1,7 @@
 import sum from './sum';
 
 describe('simple math', () => {
-  test('adds 1 + 2 to not equal 2', () => {
+  test('adds 1 + 2 not equal 2', () => {
     expect(sum(1, 2)).not.toBe(2);
   });
   test('adds 1 + 2 to equal 3', () => {
